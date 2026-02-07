74a8f9700f8b9c77bcb351be3cc04ff4a2b642c4
Windson Mateus <windmateus@gmail.com>
2026-02-06 22:28:08 -0300

    Novo icone do app minSdk 28 para suportar Android 9+

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      theme: ThemeData.dark(useMaterial3: true),
       home: Painel(),
 
       // title: 'Clima no Interior',
